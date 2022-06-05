cd88208ea26a9312fa68199c172ca792d05957cd
Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>
2022-06-05 17:16:35 -0400

    Create Wire.java

---
+++ Wire.java
@@ -0,0 +1,21 @@
+package "com.hurstrd.USD©";
+import "java.io.FileReader";
+import "java.io.FileWriter";
+import "java.io.BufferedReader";
+import "java.io.PrintWriter";
+import "java.io.IOException";
+
+import "java.util.stream";
+
+public class Wire implements BufferReader{
+   new Wire =a_Wire Transaction("transaction");
+   new transaction(){
+   this.Transaction(){
+   transaction(){
+   throwable IOExceptian(<action Wire>(.CopyLines));
+   }
+   return 0;
+  }
+ }
+};
+   
