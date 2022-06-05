a4371ced85081aafba91080b32dcb35f7bed4e53
Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>
2022-06-05 17:51:30 -0400

    Update and rename Stream.java to Stream_Writer.java

---
+++ Stream_Writer.java
@@ -0,0 +1,12 @@
+compact1,compact2,compact3;
+public.java.io.Buffer."Stream_Writer";
+private.class.BufferedInputStream."BufferedInputStream";
+public.java.lang.object."BufferReader";
+private.java.io.InputStream."InputStream";
+private.java.io.FilterInputStream."BufferedInputStream";
+private.java.io.BufferedInputStream."FilterInputStream";
+assert.Interfaces:"Closeable","AutoCloseable";
+
+public.static.void.main\Strings.Args[]\:
+this.inputStream.new.BufferedReader(new.FileReader("USD©\Wire.java",.transaction));
+this.outputStream.new.BufferedWriter(new.FileWriter(inputStream.BufferReader));
