b97dbe6400c104812bca3f90c243c16009dc73ad
Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>
2022-06-05 16:00:08 -0400

    Update and rename $USD© to Security.java

---
+++ Security.java
@@ -0,0 +1,5 @@
+public interface SECURITY{
+   public BOND(String total_Amount, String total_value,String USD[]);
+  new BOND = a_BOND a_bond(){
+    this.Security()<"USD[]"> {String US_Dollar,String value};
+  
