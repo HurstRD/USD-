beb1523cb421c0097d47eb1ad7bba6ba6bf2d116
Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>
2022-09-10 17:50:58 -0400

    Create Config.tf
    Signed-off-by: Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>

---
+++ Config.tf
@@ -0,0 +1,5 @@
+terraform plan -input=false -destroy
+  shell: /usr/bin/bash --noprofile --norc -e -o pipefail {0}
+  env:
+    TERRAFORM_CLI_PATH: /home/runner/work/_temp/806a73b7-e07f-4c8f-8e2f-0c55b7981fc6
+/home/runner/work/_temp/806a73b7-e07f-4c8f-8e2f-0c55b7981fc6/terraform-bin plan -input=false
